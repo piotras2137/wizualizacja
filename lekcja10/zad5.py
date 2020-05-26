321bb19719e1cb5f9ce153f2319f472dc0bc1f30
Piotr Martyniuk <piotras2137@gmail.com>
2020-05-26 10:46:11 +0200

    zrobilem zadanie 5

--- lekcja10/zad5.py
+++ lekcja10/zad5.py
@@ -1,9 +1,20 @@
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-#tu se musze skonczycz ale mi sie nie chce
+import xlrd
+import openpyxl
 
-plt.xlabel('sepal length')
-plt.ylabel('sepal width')
+df = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data", names=["SepalLengthCm", "SepalWidthCm", "PetalLengthCm", "PetalWidthCm", "Species"])
+
+x = df["SepalLengthCm"]
+y = df["SepalWidthCm"]
+
+data = {"x": x,
+        "y": y,
+        "c": np.random.randint(0, 50, 150),
+        "s": abs(x.subtract(y))}
+
+plt.scatter('x', 'y', c='c', s='s', data=data)
+plt.xlabel("Sepal Length")
+plt.ylabel("Sepal Width")
 plt.show()
